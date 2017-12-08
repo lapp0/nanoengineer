43319737a8b6b7b80ee8351cc6029ad14cd641ab
andrew <andrew>
2017-12-08 09:05:39 -0500

    fix argparser

--- cad/src/main.py
+++ cad/src/main.py
@@ -14,28 +14,26 @@ def start_application(verbosity, data_dir, recursion_limit, dev_mode):
     logger.info('starting NanoEngineer')
     logger.info('using Python: {}'.format(sys.version))
     logger.info('using the data path: {}'.format(data_dir))
-    
+
     logger.info('Setting recursion limit to {}'.format(recursion_limit))
     sys.setrecursionlimit(recursion_limit)
 
 
 def run():
+    # TODO: handle mmp file here? why can an mmp file be passed?
+    # TODO: nosplash
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2], default=1,
                         help='set log verbosity (higher is more verbose)')
-    parser.add_argument("-d", "--data-dir", type=int, default=os.getcwd(),
+    parser.add_argument("-d", "--data-dir", type=str, default=os.getcwd(),
                         help='path storing the data')
     parser.add_argument("-r", "--recursion-limit", type=int, default=5000,
                         help='specify the system recursion limit')
     parser.add_argument("-D", "--dev-mode", type=int,
                         help='enable devleoper mode features')
-    # TODO: handle mmp file here? why can an mmp file be passed?
-    # TODO: nosplash
-
-    parser.parse_args()
-
-    start_application(parser.verbosity, parser.data_dir, parser.recursion_limit, parser.dev_mode)
-    start_application()
+    args = parser.parse_args()
+    start_application(args.verbosity, args.data_dir, args.recursion_limit, args.dev_mode)
 
 
 if __name__ == '__main__':
